d706a2d076c725c9b096962782a5de5cf3dc887d
Michal Sojka <michal.sojka@cvut.cz>
2023-04-16 09:48:13 +0200

    Make symlinks relative

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,40 @@ use cli::{Cli, Commands};
 use dirs::home_dir;
 use std::env::current_dir;
 use std::fs::remove_file;
+use std::os::unix::fs::symlink;
+use std::path::{Component, PathBuf};
 use std::{
     fs::{self, create_dir_all},
     path::Path,
 };
 use walkdir::{DirEntry, WalkDir};
+
+/// Converts absolute `path` to be relative to absolute `to` path.
+// From: https://github.com/uutils/coreutils/blob/084510e499ce8a8d9e7da96731e33f671070baab/src/uucore/src/lib/features/fs.rs#L562
+// License: MIT
+pub fn make_path_relative_to<P1: AsRef<Path>, P2: AsRef<Path>>(path: P1, to: P2) -> PathBuf {
+    let path = path.as_ref();
+    let to = to.as_ref();
+    let common_prefix_size = path
+        .components()
+        .zip(to.components())
+        .take_while(|(first, second)| first == second)
+        .count();
+    let path_suffix = path
+        .components()
+        .skip(common_prefix_size)
+        .map(|x| x.as_os_str());
+    let mut components: Vec<_> = to
+        .components()
+        .skip(common_prefix_size)
+        .map(|_| Component::ParentDir.as_os_str())
+        .chain(path_suffix)
+        .collect();
+    if components.is_empty() {
+        components.push(Component::CurDir.as_os_str());
+    }
+    components.iter().collect()
+}
 
 fn walk_shadow_files<F>(shadow_dir: &Path, f: F) -> anyhow::Result<()>
 where
@@ -29,16 +58,26 @@ fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     let home_dir = home_dir().expect("home directory should be defined");
-    let env_base = home_dir.join(Path::new("nix/conf/env"));
-    let shadow_dir = env_base.join(current_dir()?.strip_prefix(home_dir)?);
+    let shadow_base = home_dir.join(Path::new("nix/conf/env")).canonicalize()?;
+    let shadow_dir = shadow_base.join(current_dir()?.strip_prefix(home_dir)?);
 
     match &cli.command {
         Commands::Add { files } => {
             for f in files {
                 let shadow = shadow_dir.join(f);
-                create_dir_all(shadow.parent().unwrap())?;
-                fs::rename(f, &shadow)?; // FIXME: Don't require same filesystem
-                std::os::unix::fs::symlink(&shadow, f)?;
+                let src = PathBuf::from(f);
+                if src.is_symlink() {
+                    bail!("{} is already a symlink", f);
+                }
+                let parent = shadow.parent().unwrap();
+                create_dir_all(parent)
+                    .with_context(|| format!("Creating directory {}", parent.display()))?;
+                let srcc = src
+                    .canonicalize()
+                    .with_context(|| format!("Canonicalizing {}", src.display()))?;
+                fs::rename(f, &shadow) // FIXME: Don't require same filesystem
+                    .with_context(|| format!("Renaming {} to {}", f, shadow.display()))?;
+                symlink(make_path_relative_to(&shadow, srcc.parent().unwrap()), srcc)?;
             }
         }
         Commands::Ls {} => walk_shadow_files(&shadow_dir, |e| {
@@ -53,17 +92,26 @@ fn main() -> anyhow::Result<()> {
                 if !shadow.is_file() {
                     bail!("`{}` is not a file", shadow.display());
                 }
-                if f.is_symlink() && f.is_file() && fs::read_link(f)? == shadow {
-                    remove_file(f).with_context(|| format!("Removing `{}`", f.display()))?;
-                    fs::rename(&shadow, f).with_context(|| {
-                        format!("Moving `{}` to `{}`", shadow.display(), f.display())
-                    })?;
-                } else {
-                    bail!(
-                        "Not removing `{}` because it's not a symlink pointing to file `{}`",
-                        f.display(),
-                        shadow.display()
-                    );
+                if f.is_symlink() && f.is_file() {
+                    let dst = fs::read_link(f)?;
+                    if dst
+                        .canonicalize()
+                        .with_context(|| format!("Canonicalizing {}", dst.display()))?
+                        == shadow
+                            .canonicalize()
+                            .with_context(|| format!("Canonicalizing {}", shadow.display()))?
+                    {
+                        remove_file(f).with_context(|| format!("Removing `{}`", f.display()))?;
+                        fs::rename(&shadow, f).with_context(|| {
+                            format!("Moving `{}` to `{}`", shadow.display(), f.display())
+                        })?;
+                    } else {
+                        bail!(
+                            "Not removing `{}` because it's not a symlink pointing to file `{}`",
+                            f.display(),
+                            shadow.display()
+                        );
+                    }
                 }
             }
         }
@@ -100,7 +148,7 @@ fn restore(shadow_dir: &std::path::PathBuf, force: bool) -> Result<(), anyhow::E
             }
         }
         if link.exists() {
-            if link.is_symlink() && fs::read_link(link)? == e.path() {
+            if link.is_symlink() && fs::read_link(link)?.canonicalize()? == e.path() {
                 return Ok(());
             }
             if force {
